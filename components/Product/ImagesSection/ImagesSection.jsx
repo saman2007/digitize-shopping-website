ef9fca4e4940ca281e093636a41811393dbd93d5
Saman <samanahrari@yahoo.com>
2022-09-21 22:48:44 +0430

    added some exceptions

--- components/Product/ImagesSection/ImagesSection.jsx
+++ components/Product/ImagesSection/ImagesSection.jsx
@@ -6,22 +6,28 @@ const ImagesSection = ({ images }) => {
   const [selectedImage, setSelectedImage] = useState(0);
 
   return (
-    <div className="md:m-0 order-1 m-auto w-full very-sm:w-[300px] flex flex-col p-[5px] h-[490px]">
-      <MainImage
-        src={images[selectedImage].src}
-        alt={images[selectedImage].alt}
-      />
-      <div className="w-full overflow-x-auto pb-[2px] flex h-auto">
-        {images.map((img, index) => (
-          <SubImage
-            {...img}
-            key={index}
-            isSelected={index === selectedImage}
-            selectImage={setSelectedImage.bind(this, index)}
+    <>
+      {images.length !== 0 ? (
+        <div className="md:m-0 order-1 m-auto w-full very-sm:w-[300px] flex flex-col p-[5px] h-[490px]">
+          <MainImage
+            src={images[selectedImage].src}
+            alt={images[selectedImage].alt}
           />
-        ))}
-      </div>
-    </div>
+          <div className="w-full overflow-x-auto pb-[2px] flex h-auto">
+            {images.map((img, index) => (
+              <SubImage
+                {...img}
+                key={index}
+                isSelected={index === selectedImage}
+                selectImage={setSelectedImage.bind(this, index)}
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
--- components/Product/ReviewProduct/Review.jsx
+++ components/Product/ReviewProduct/Review.jsx
@@ -25,27 +25,33 @@ const Review = ({ htmlStrings }) => {
   };
 
   return (
-    <div>
-      <div
-        className="text-slate-800 overflow-y-hidden transition-all duration-500"
-        style={{ maxHeight: "185px" }}
-        ref={reviewEl}
-      >
-        <h2 className="text-[24px] font-yekan-bl mb-[30px]">
-          نقد و بررسی این محصول
-        </h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: htmlStrings }}
-          className="overflow-y-hidden"
-        ></div>
-      </div>
-      <p
-        onClick={showMoreOrLess}
-        className="mt-[10px] text-orange-700 cursor-pointer w-fit"
-      >
-        {showMore ? "مشاهده کمتر" : "مشاهده بیشتر"}
-      </p>
-    </div>
+    <>
+      {htmlStrings !== null ? (
+        <div>
+          <div
+            className="text-slate-800 overflow-y-hidden transition-all duration-500"
+            style={{ maxHeight: "185px" }}
+            ref={reviewEl}
+          >
+            <h2 className="text-[24px] font-yekan-bl mb-[30px]">
+              نقد و بررسی این محصول
+            </h2>
+            <div
+              dangerouslySetInnerHTML={{ __html: htmlStrings }}
+              className="overflow-y-hidden"
+            ></div>
+          </div>
+          <p
+            onClick={showMoreOrLess}
+            className="mt-[10px] text-orange-700 cursor-pointer w-fit"
+          >
+            {showMore ? "مشاهده کمتر" : "مشاهده بیشتر"}
+          </p>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
